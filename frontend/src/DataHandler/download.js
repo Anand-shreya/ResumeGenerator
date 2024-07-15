6dfab3ce2fe7f060740a554cc1de22c0bcad7ac1
Shreya Anand <106630585+Anand-shreya@users.noreply.github.com>
2024-07-15 18:41:57 +0530

    Update download.js

--- frontend/src/DataHandler/download.js
+++ frontend/src/DataHandler/download.js
@@ -1,62 +1,15 @@
-// //Handle Downloading of final REsume
+//Handle Downloading of final REsume
 
-// import React from 'react'
-// import axios from 'axios';
-
-// //Function for downloading RESUME
-// const downloadFile = async () => {
-//   try {
-//     // const response = await axios.get('http://localhost:8000/download', {
-//       const response = await axios.get('https://resumegenerator-952f.onrender.com/download', {
-//       responseType: 'blob',
-//     });
-//     const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
-//     const link = document.createElement('a');
-//     link.href = downloadUrl;
-//     link.setAttribute('download', 'resume.pdf');
-//     document.body.appendChild(link);
-//     link.click();
-//     link.remove();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-
-// const Download = () => {
-
-//     const handleUploadClick = async()=>{
-//         const res = await downloadFile();
-//     }
-
-// return (
-//     <div>
-//       <button className='download_btn' onClick={handleUploadClick}>Download</button>      {/*//Download Button*/}
-//     </div>
-// )
-// }
-
-// export default Download;
-
-
-
-
-
-
-
-import React from 'react';
+import React from 'react'
 import axios from 'axios';
 
-// Function for downloading RESUME
+//Function for downloading RESUME
 const downloadFile = async () => {
   try {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl = 'https://resumegenerator-952f.onrender.com/download';
-    
-    const response = await axios.get(proxyUrl + targetUrl, {
+    // const response = await axios.get('http://localhost:8000/download', {
+      const response = await axios.get('https://resumegenerator-952f.onrender.com/download', {
       responseType: 'blob',
     });
-
     const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = downloadUrl;
@@ -65,20 +18,67 @@ const downloadFile = async () => {
     link.click();
     link.remove();
   } catch (error) {
-    console.error('Error downloading file:', error);
+    console.error(error);
   }
 };
 
-const Download = () => {
-  const handleUploadClick = async () => {
-    await downloadFile();
-  };
 
-  return (
+const Download = () => {
+
+    const handleUploadClick = async()=>{
+        const res = await downloadFile();
+    }
+
+return (
     <div>
-      <button className='download_btn' onClick={handleUploadClick}>Download</button>
+      <button className='download_btn' onClick={handleUploadClick}>Download</button>      {/*//Download Button*/}
     </div>
-  );
-};
+)
+}
 
 export default Download;
+
+
+
+
+
+
+
+// import React from 'react';
+// import axios from 'axios';
+
+// // Function for downloading RESUME
+// const downloadFile = async () => {
+//   try {
+//     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+//     const targetUrl = 'https://resumegenerator-952f.onrender.com/download';
+    
+//     const response = await axios.get(proxyUrl + targetUrl, {
+//       responseType: 'blob',
+//     });
+
+//     const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
+//     const link = document.createElement('a');
+//     link.href = downloadUrl;
+//     link.setAttribute('download', 'resume.pdf');
+//     document.body.appendChild(link);
+//     link.click();
+//     link.remove();
+//   } catch (error) {
+//     console.error('Error downloading file:', error);
+//   }
+// };
+
+// const Download = () => {
+//   const handleUploadClick = async () => {
+//     await downloadFile();
+//   };
+
+//   return (
+//     <div>
+//       <button className='download_btn' onClick={handleUploadClick}>Download</button>
+//     </div>
+//   );
+// };
+
+// export default Download;
